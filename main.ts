934511ba73841a52e531cdf5ead2aeb4eb3c5c9c
Francisco Manuel Garcia Vallejo <franciscogarciavallejo@gmail.com>
2023-06-15 05:44:20 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 60)
+    } else {
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 60)
+        }
+    }
+})
